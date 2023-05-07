9879aabdd959bc879289c43154642a578959e3b1
Poorna Soysa <poornapiumal.soysa@gmail.com>
2023-05-07 12:37:00 +0530

    Up to Discount.Grpc and consume discount.grpc from basket

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,21 @@
+using Basket.API.GrpcServices;
+using Basket.API.Repositories;
+using Discount.Grpc.Protos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetSection("CacheSettings:ConnectionString").Value;
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o =>{
+    o.Address = new Uri(builder.Configuration.GetSection("GrpcSettings:DiscountUrl").Value);
+});
+builder.Services.AddScoped<DiscountGrpcService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
